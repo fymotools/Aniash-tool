af247085dea183adef8b345ca8c3358a2bdb9de3
fymotools <fymotools@gmail.com>
2025-06-25 07:39:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-import React from 'react'; import ReactDOM from 'react-dom'; import App from './App'; ReactDOM.render(<App />, document.getElementById('root'));
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Tailwind CSS ya custom CSS ke liye
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
