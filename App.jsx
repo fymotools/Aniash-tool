60da7eb04813510e6ac31a55755400b76941bd76
fymotools <fymotools@gmail.com>
2025-06-26 09:39:43 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,42 +1,54 @@
-// src/App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import TaxTools from './pages/TaxTools';
-import HealthTools from './pages/HealthTools';
-import WritingTools from './pages/WritingTools';
-import SeoTools from './pages/SeoTools';
-import ImageTools from './pages/ImageTools';
-import DailyTools from './pages/DailyTools';
-import Donate from './pages/Donate';
-import Blog from './pages/Blog';
+// src/App.jsx 
 
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { LanguageProvider } from "./context/LanguageContext";
+
+// Layout & Shared
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// Pages
+import Home from "./pages/Home";
+import TaxTools from "./pages/TaxTools";
+import HealthTools from "./pages/HealthTools";
+import WritingTools from "./pages/WritingTools";
+import SEOTools from "./pages/SEOTools";
+import ImageTools from "./pages/ImageTools";
+import DailyTools from "./pages/DailyTools";
+import Blog from "./pages/Blog";
+import Donate from "./pages/Donate";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+
+const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
+    <LanguageProvider>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
 
-        <main className="flex-grow px-4 py-6">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/tax-tools" element={<TaxTools />} />
-            <Route path="/health-tools" element={<HealthTools />} />
-            <Route path="/writing-tools" element={<WritingTools />} />
-            <Route path="/seo-tools" element={<SeoTools />} />
-            <Route path="/image-tools" element={<ImageTools />} />
-            <Route path="/daily-tools" element={<DailyTools />} />
-            <Route path="/donate" element={<Donate />} />
-            <Route path="/blog" element={<Blog />} />
-          </Routes>
-        </main>
+          <main className="flex-1">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/tax-tools" element={<TaxTools />} />
+              <Route path="/health-tools" element={<HealthTools />} />
+              <Route path="/writing-tools" element={<WritingTools />} />
+              <Route path="/seo-tools" element={<SEOTools />} />
+              <Route path="/image-tools" element={<ImageTools />} />
+              <Route path="/daily-tools" element={<DailyTools />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/donate" element={<Donate />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </main>
 
-        <Footer />
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
+    </LanguageProvider>
   );
-}
+};
 
 export default App;
